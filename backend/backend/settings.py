14eb801a5f9faf04bc84ba1951bfe035428ef0f8
gameonceagain <gethiredinminutes@gmail.com>
2024-07-08 15:00:00 +0530

    intial

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,11 +83,15 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 #
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PWD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
 
 
 RQ_QUEUES = {
